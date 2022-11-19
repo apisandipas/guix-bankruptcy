408764832b526768ba9f2b7e9d0bb5129a341abf
Bryan Paronto <bryan@kindsys.us>
2022-11-19 05:37:08 -0600

    add missing user module

--- cablecar/configs.scm
+++ cablecar/configs.scm
@@ -197,4 +197,25 @@
     #:templates `(fundamental-mode
                   ,#~""
                   (t (format-time-string "%Y-%m-%d"))))
+
+   (feature-emacs
+    #:default-application-launcher? #t
+    #:additional-elisp-packages
+    (append
+     (list emacs-dirvish)
+     (strings->packages
+      "emacs-elfeed" "emacs-hl-todo"
+      "emacs-yasnippet"
+      ;; "emacs-company"
+      "emacs-consult-dir"
+      ;; "emacs-all-the-icons-completion" "emacs-all-the-icons-dired"
+      "emacs-kind-icon"
+      "emacs-nginx-mode" "emacs-yaml-mode"
+      ;; "emacs-lispy"
+      "emacs-ytdl"
+      "emacs-multitran"
+      "emacs-minimap"
+      "emacs-ement"
+      "emacs-restart-emacs"
+      "emacs-org-present")))
    ))
